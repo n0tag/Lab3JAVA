5b669d25db9009b92b58b1b72778c87720101ba8
n0tag <56114701+n0tag@users.noreply.github.com>
2021-07-01 01:17:19 +0200

    Add files via upload

---
+++ Lab3/Lab3/src/sample/Controller.java
@@ -0,0 +1,20 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class Controller
+{
+    public Button btn1;
+    public Text txtLicznik;
+    public Text txtLicznik0;
+    int clickCounter = 0;
+
+    public void OnAction(ActionEvent actionEvent)
+    {
+        ++clickCounter;
+        txtLicznik1.setText(Integer.toString(clickCounter));
+        System.out.println("Kliknięcie nr: " + txtLicznik0.getText());
+    }
+}
